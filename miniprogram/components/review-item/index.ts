502d80fac3b1ecf3cdd7e9f45e6d27598010c357
james-byu <byu.jiang.02@gmail.com>
2026-07-03 10:46:51 +0800

    ui: add review reply trigger

--- miniprogram/components/review-item/index.ts
+++ miniprogram/components/review-item/index.ts
@@ -4,12 +4,14 @@ Component({
     showAlbum: { type: Boolean, value: false },
   },
   methods: {
-    onTap() {
-      this.triggerEvent('tap', { review: this.properties.review })
-    },
+    onTap() { this.triggerEvent('tap', { review: this.properties.review }) },
     onLike() {
       const review = this.properties.review as any
       this.triggerEvent('like', { reviewId: review && review._id })
+    },
+    onReply() {
+      const review = this.properties.review as any
+      this.triggerEvent('reply', { reviewId: review && review._id, userName: review && review.userName })
     },
   },
 })
