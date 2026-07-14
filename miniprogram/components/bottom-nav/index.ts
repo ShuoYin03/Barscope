f90fa8cac4e35f4a8f31ad352b7e80265a73b29f
james-byu <byu.jiang.02@gmail.com>
2026-07-14 16:55:42 +0800

    feat: replace favorites tab with features

--- miniprogram/components/bottom-nav/index.ts
+++ miniprogram/components/bottom-nav/index.ts
@@ -7,7 +7,7 @@ Component({
       { icon: '/assets/icons/nav/home.svg',    iconActive: '/assets/icons/nav/home-active.svg',    text: '首页', path: '/pages/home/index' },
       { icon: '/assets/icons/nav/charts.svg',  iconActive: '/assets/icons/nav/charts-active.svg',  text: '榜单', path: '/pages/charts/index' },
       { icon: '/assets/icons/nav/compass.svg', iconActive: '/assets/icons/nav/compass-active.svg', text: '发现', path: '/pages/discover/index' },
-      { icon: '/assets/icons/nav/heart.svg',   iconActive: '/assets/icons/nav/heart-active.svg',   text: '收藏', path: '/pages/favorites/index' },
+      { icon: '/assets/icons/nav/features.svg',iconActive: '/assets/icons/nav/features-active.svg',text: '专题', path: '/pages/features/index' },
       { icon: '/assets/icons/nav/user.svg',    iconActive: '/assets/icons/nav/user-active.svg',    text: '我的', path: '/pages/profile/index' },
     ],
   },
