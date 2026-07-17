96e3982b1f053bc70b414c27aa39a20e9a1a4d26
james-byu <byu.jiang.02@gmail.com>
2026-07-17 16:40:27 +0800

    feat(ios): add Soundive Xcode project

---
+++ ios/Soundive/Soundive/SoundiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundiveApp.swift
+//  Soundive
+//
+//  Created by jiang on 2026/7/17.
+//
+
+import SwiftUI
+
+@main
+struct SoundiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
